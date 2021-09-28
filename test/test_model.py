212ed653528d257b36a360609358abf495a1becb
Timothy Clifford <tcliff30@terpmail.umd.edu>
2021-09-28 13:05:57 -0400

    boilerplate for Model attempt 5

--- test/test_model.py
+++ test/test_model.py
@@ -7,7 +7,7 @@ def test_model_init():
 
 def test_model_evaluate():
   model = Model(100,50)
-  simple_movement = Model.evaluate(Model)
+  simple_movement = Model.evaluate(Model, [0,0])
   assert(len(simple_movement) == 7)
   for i in range(7):
     assert(simple_movement[i] >= 0 and simple_movement[i] <= 1)
