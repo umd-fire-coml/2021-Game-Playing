f6f3fdaed2c9c7da6cca5449ea59c261492c18fe
Timothy Clifford <tcliff30@terpmail.umd.edu>
2021-09-28 13:03:27 -0400

    boilerplate for Model attempt 4

--- test/src/Model.py
+++ test/src/Model.py
@@ -1,9 +1,8 @@
 class Model:
 
-	def __init__(self, image, height, width):
-            self.image = image
+	def __init__(self, height, width):
             self.height = height
             self.width = width
 
-	def evaluate(self):
+	def evaluate(self, image):
             return [0.4,0.1,0.1,0.1,0.1,0.1,0.1]
--- test/test_model.py
+++ test/test_model.py
@@ -1,4 +1,13 @@
 from src.Model import Model
 
-def test_model():
-  assert(Model.evaluate(Model)[0] == 0.4)
+def test_model_init():
+  model = Model(100,50)
+  assert(model.height == 100)
+  assert(model.width == 50)
+
+def test_model_evaluate():
+  model = Model(100,50)
+  simple_movement = Model.evaluate(Model)
+  assert(len(simple_movement) == 7)
+  for i in range(7):
+    assert(simple_movement[i] >= 0 and simple_movement[i] <= 1)
