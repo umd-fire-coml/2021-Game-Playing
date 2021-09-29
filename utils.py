7509514a04bfe96fe6ae01dd2a32bfb8775db437
Rithvik Bobbili <rithvikbobbili@gmail.com>
2021-09-29 13:47:13 -0400

    utils class boilerplate

---
+++ utils.py
@@ -0,0 +1,14 @@
+class utils:
+
+    def __init__(self, env):
+        self.env = "placeholder"
+        self.steps = 0
+
+    def reset_env():
+        None
+
+    def get_state():
+        return "environment state"
+
+    def take_step():
+        None
