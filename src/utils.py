76a17cc12413f4224d1e3e6209db094fb9ec15f2
Rithvik Bobbili <rithvikbobbili@gmail.com>
2021-10-01 11:57:10 -0400

    added basic functionality for utils class, functional in colab

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,55 @@
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython import display as ipythondisplay
+from tqdm import tqdm
+from pyvirtualdisplay import Display
+
 class Utils:
 
-    def __init__(self, env):
-        self.env = "placeholder"
-        self.steps = 0
+    def __init__(self, level):
+        # add level selection
+        self.env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
+
+        self.env = JoypadSpace(self.env, SIMPLE_MOVEMENT)
+        self.obs
+        self.reward
+        self.done
+        self.info
 
     def reset_env(self):
-        None
+        self.env.reset()
 
     def get_state(self):
-        state = "placeholder state"
-        return state
+        return self.info
 
-    def take_step(self):
-        None
+    def get_reward(self):
+        return self.reward
+
+    def take_step(self, action):
+        self.obs, self.reward, self.done, self.info = self.env.step(action)
+
+    def simulate_steps(self, step_count):
+        display = Display(visible = 0, size = (400, 300))
+        display.start()
+        action_meanings = self.env.get_action_meanings()
+        self.env.reset()
+
+        for i in tqdm(range(step_count)):
+          action = 2 # selecting A and right random: env.step(env.action_space.sample())
+          obs, reward, done, info = self.env.step(action)
+          #print(info)
+          print("\nAction: %s" % (action_meanings[action]))
+          print("Reward: {:.2f}".format(reward))
+          screen = self.env.render(mode = 'rgb_array')
+
+          plt.imshow(screen)
+          ipythondisplay.clear_output(wait = True)
+          ipythondisplay.display(plt.gcf())
+
+          if done:
+              break
+        
+        ipythondisplay.clear_output(wait = True)
