5c4076eb3cdb0555179ea803a427f332bd0971bf
Timothy Clifford <tcliff30@terpmail.umd.edu>
2021-10-04 18:48:53 -0400

    yer

--- test/test_env_reqs.py
+++ test/test_env_reqs.py
@@ -2,10 +2,8 @@ import requirements.txt
 import test_requirements.txt
 
 def test_tensorflow():
-    assert(true)
+    assert(tensorflow.__version__() == "2.6.0")
 
 def test_numpy():
-    assert(true)
-
-def test_matplotlib():
-    assert (true)
+    array = numpy.array([1,2,3])
+    assert(len(array) == 3)
