2bb1d760c7c855f433d665240152a22eb1b7a836
Timothy Clifford <tcliff30@terpmail.umd.edu>
2021-10-04 17:53:30 -0400

    test

---
+++ test/test_env_reqs.py
@@ -0,0 +1,11 @@
+import requirements.txt
+import test_requirements.txt
+
+def test_tensorflow():
+    assert(true)
+
+def test_numpy():
+    assert(true)
+
+def test_matplotlib():
+    assert true()
