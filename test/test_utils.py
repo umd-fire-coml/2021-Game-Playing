80e7eb067b08861d78d7b532f06cf8578f6f5f06
Rithvik Bobbili <rithvikbobbili@gmail.com>
2021-10-06 11:51:38 -0400

    deleted duplicate file

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,16 +1,16 @@
 from src.utils import SuperMarioBros_Dataset
 
 def test_env_setup():
-    preprocessor = SuperMarioBros_Dataset("1-1", "v0")
-    preprocessor.reset_env()
-    preprocessor.take_step(1)
-    assert(preprocessor.get_state('coins') == 0)
-    assert(preprocessor.get_state('stage') == 1)
-    assert(preprocessor.get_state('world') == 1)
+    env = SuperMarioBros_Dataset("1-1", "v0")
+    env.reset_env()
+    env.take_step(1)
+    assert(env.get_state('coins') == 0)
+    assert(env.get_state('stage') == 1)
+    assert(env.get_state('world') == 1)
 
-    preprocessor2 = SuperMarioBros_Dataset("2-2", "v0")
-    preprocessor2.reset_env()
-    preprocessor2.take_step(1)
-    assert(preprocessor2.get_state('coins') == 0)
-    assert(preprocessor2.get_state('stage') == 2)
-    assert(preprocessor2.get_state('world') == 2)
+    env2 = SuperMarioBros_Dataset("2-2", "v0")
+    env2.reset_env()
+    env2.take_step(1)
+    assert(env2.get_state('coins') == 0)
+    assert(env2.get_state('stage') == 2)
+    assert(env2.get_state('world') == 2)
--- test/utils_tests.py
+++
@@ -1,5 +0,0 @@
-from src.utils import Utils
-
-def test_boilerplate():
-    preprocessor = Utils()
-    assert(preprocessor.get_state() == "environment state")
