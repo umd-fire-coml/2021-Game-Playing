d7a8e3822bc5d8b4df8dfe135742eb4eb4a1436d
Timothy Clifford <tcliff30@terpmail.umd.edu>
2021-10-10 19:02:43 -0400

    THIS SHOULD ACTUALLY WORK

--- test/test_env_reqs.py
+++ test/test_env_reqs.py
@@ -7,7 +7,7 @@ def test_tensorflow():
     assert(tensorflow.__version__ == "2.6.0")
 
 def test_numpy():
-    assert(numpy.__version__ == "1.19.0")
+    assert(numpy.__version__ == "1.19.5")
 
 def test_matplotlib():
     assert(matplotlib.__version__ == "3.4.3")
