dad6f43765be197095ef2848eb3775156bd55753
JonathanGinsberg <89946716+JonathanGinsberg@users.noreply.github.com>
2021-10-19 12:48:39 -0400

    Create test_experience_replay.py

---
+++ test/test_experience_replay.py
@@ -0,0 +1,17 @@
+from src.experience_replay import ReplayMemory
+import numpy as np
+import random
+
+mem = ReplayMemory()
+    
+#Populate experience buffer
+def test_experience_replay():
+  for i in range(0,256):
+    frame = np.random.randint(255, size=(240, 256))
+    action = np.random.randint(4)
+    reward = np.random.randint(2)
+    terminal = np.random.choice(a=[False, False, False, False, False, False, False, False, True])
+    
+    assert(mem.add(action, reward, frame, terminal))
+        
+assert(batch = mem.getMinibatch())
