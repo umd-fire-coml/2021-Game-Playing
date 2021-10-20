007b5c0f2c0b0b9e5e646a0153b33ca53361219f
JonathanGinsberg <89946716+JonathanGinsberg@users.noreply.github.com>
2021-10-20 14:45:22 -0400

    Update test_experience_replay.py

--- test/test_experience_replay.py
+++ test/test_experience_replay.py
@@ -4,8 +4,8 @@ import random
     
 #Populate experience buffer
 def test_experience_replay():
-    mem = ReplayMemory()
-    for i in range(0,50000):
+    mem = ReplayMemory(10000, 500)
+    for i in range(0,500):
         frame = np.random.randint(255, size=(240, 256))
         action = np.random.randint(4)
         reward = np.random.randint(2)
