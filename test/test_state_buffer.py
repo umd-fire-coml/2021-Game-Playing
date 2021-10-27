2c5af9ce03b6e49da0e3c9e588aebd9f46951410
JonathanGinsberg <89946716+JonathanGinsberg@users.noreply.github.com>
2021-10-27 13:40:00 -0400

    Create test_state_buffer.py

---
+++ test/test_state_buffer.py
@@ -0,0 +1,22 @@
+from src.state_buffer import StateBuffer
+import numpy as np
+
+def test_state_buffer():
+    buf = StateBuffer()
+    
+    #Populate experience buffer
+    for i in range(0,9):
+        frame = np.random.randint(255, size=(240, 256))
+        action = np.random.randint(4)
+        reward = np.random.randint(2)
+        terminal = np.random.choice(a=[False, False, False, False, False, False, False, False, True])
+    
+        buf.add(frame)
+        
+        state = buf.get_state()
+
+        if i == 5:
+            buf.reset()
+    assert(len(state) == 240)
+    assert(len(state[0]) == 256)
+    assert(len(state[0][0]) == 4)
