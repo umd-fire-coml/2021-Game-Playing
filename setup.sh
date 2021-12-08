aab97984c8321ae24e8149c0dbb0e2277fd9a548
Rithvik Bobbili <rithvikbobbili@gmail.com>
2021-12-08 12:32:16 -0500

    trying modified setup for cv2 dependencies

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ python3 -m venv supermariobros-env
 source supermariobros-env/bin/activate
 
 # Installation Reqs
+apt-get update
+apt-get install ffmpeg libsm6 libxext6  -y
 pip install -r requirements.txt
 pip install -r test-requirements.txt
