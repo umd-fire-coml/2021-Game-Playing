02f127e4fb9e6dec8fa331f4501a441f27162de8
Rithvik Bobbili <rithvikbobbili@gmail.com>
2021-12-08 16:29:44 -0500

    updated tests

--- test/test_model.py
+++ test/test_model.py
@@ -1,13 +1,20 @@
-from src.Model import Model
+from src.model import Model
+from src.dataset import SuperMarioBros_Dataset
 
 def test_model_init():
-  model = Model(100,50)
-  assert(model.height == 100)
-  assert(model.width == 50)
+  env = SuperMarioBros_Dataset("1-1", "v0")
+  model = Model(env, "1-1", "v0")
 
-def test_model_evaluate():
-  model = Model(100,50)
-  simple_movement = Model.evaluate(Model, [0,0])
-  assert(len(simple_movement) == 7)
-  for i in range(7):
-    assert(simple_movement[i] >= 0 and simple_movement[i] <= 1)
+def test_load_ckpt():
+  env = SuperMarioBros_Dataset("1-1", "v0")
+  model = Model(env, "1-1", "v0")
+  model.load_ckpt('model.ckpt-4000')
+
+def test_train():
+  env = SuperMarioBros_Dataset("1-1", "v0")
+  model = Model(env, "1-1", "v0")
+  model.train('model.ckpt-4000', '500')
+
+def run_model():
+  env = SuperMarioBros_Dataset("1-1", "v0")
+  model = Model(env, "1-1", "v0")
