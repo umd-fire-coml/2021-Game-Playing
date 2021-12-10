8d732dc4d0ea0354b76f9927cab3c56623d6c60e
Rithvik Bobbili <rithvikbobbili@gmail.com>
2021-12-09 21:16:10 -0500

    shrunk train test steps

--- test/test_model.py
+++ test/test_model.py
@@ -10,16 +10,16 @@ def test_train():
   env = SuperMarioBros_Dataset("1-1", "v0")
   model = Model(env, "1-1", "v0")
   model.train(['--env', 'SuperMarioBros-1-1-v0',
-                            '--num_steps_train', '10000',
+                            '--num_steps_train', '100',
                             '--save_ckpt_step', '1000',
                             '--ckpt_dir', './ckpts',
                             '--log_dir', './logs/train',
-                            '--initial_replay_mem_size', '10000',
+                            '--initial_replay_mem_size', '1000',
                             '--frame_width', '240',
                             '--frame_height', '256',
                             '--batch_size', '16',
                             '--epsilon_step_end', '5000',
-                            '--replay_mem_size', '20000'])
+                            '--replay_mem_size', '2000'])
 
 def run_model():
   env = SuperMarioBros_Dataset("1-1", "v0")
